ce0a298dfe7d24f5c4a522adade9df6db000ca1a
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-17 21:14:32 -0400

    addded a return call to cache homepage

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,12 @@
-const FILES_TO_CACHE = ["/", "./index.html", "./index.js", "./styles.css", "/icons/icon-144x144.png", "/.icons/icon-192x192.png", "./icons/icon-512x512.png"];
+const FILES_TO_CACHE = ['/', './index.html', './index.js', './styles.css', '/manifest.webmanifest', '/icons/icon-144x144.png', '/.icons/icon-192x192.png', './icons/icon-512x512.png'];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
-self.addEventListener("install", function (evt) {
+self.addEventListener('install', function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            console.log("Your files were pre-cached successfully!");
+            console.log('Your files were pre-cached successfully!');
             return cache.addAll(FILES_TO_CACHE);
         })
     );
@@ -14,13 +14,13 @@ self.addEventListener("install", function (evt) {
 });
 
 
-self.addEventListener("activate", function (evt) {
+self.addEventListener('activate', function (evt) {
     evt.waitUntil(
         caches.keys().then(keyList => {
             return Promise.all(
                 keyList.map(key => {
                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old cache data", key);
+                        console.log('Removing old cache data', key);
                         return caches.delete(key);
                     }
                 })
@@ -30,8 +30,8 @@ self.addEventListener("activate", function (evt) {
     self.clients.claim();
 });
 
-self.addEventListener("fetch", function (evt) {
-    if (evt.request.url.includes("/api/")) {
+self.addEventListener('fetch', function (evt) {
+    if (evt.request.url.includes('/api/')) {
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(evt.request)
@@ -48,4 +48,16 @@ self.addEventListener("fetch", function (evt) {
         );
         return;
     }
+
+    evt.respondWith(
+        fetch(evt.request).catch(function () {
+            return caches.match(evt.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (evt.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/");
+                }
+            });
+        })
+    );
 });
